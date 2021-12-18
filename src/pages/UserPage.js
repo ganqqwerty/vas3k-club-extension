64543c8e8daaa3263c0d7d0d7ac377c7f68ab1a6
Yuri Katkov <katkov.juriy@gmail.com>
2021-12-19 00:55:13 +0300

    private comments

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,13 +1,31 @@
 import {Page} from "./Page";
 import {AssholesStorage} from "../storage/AssholesStorage";
+import {PrivateCommentsStorage} from "../storage/PrivateCommentsStorage";
 
 export class UserPage extends Page {
     constructor(url) {
         super(url);
+        this.user = this.getUser();
     }
 
     modifyContent() {
         this.addAssholeButton()
+        this.addPrivateCommentWidget()
+    }
+
+    addPrivateCommentWidget() {
+        const widget = document.createElement("div")
+        widget.setAttribute("class", "block")
+        const header = "<h2 class='profile-header'>Приватные комментарии</h2>"
+        const textarea = document.createElement("textarea")
+        textarea.value = PrivateCommentsStorage.getComment(this.user || "")
+        textarea.addEventListener("input", (text) => {
+            const comment = text.target.value
+            PrivateCommentsStorage.setComment(this.user, comment)
+        })
+        widget.innerHTML = header
+        widget.appendChild(textarea)
+        document.querySelector(".profile-statuses").appendChild(widget)
     }
 
     addAssholeButton() {

da27ae737026eb5173e5ec7b8d66f05db0289191
Yuri Katkov <katkov.juriy@gmail.com>
2021-12-20 00:34:55 +0300

    Move comments up

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -28,9 +28,10 @@ export class PostPage extends Page {
     movePostCommentForm() {
         const postCommentForm = document.querySelector('#post-comments-form')
         const commentsList = document.querySelector('.post-comments-list')
+        const postCommentsRules = document.querySelector('.post-comments-rules')
         const parent = commentsList.parentElement
-        postCommentForm.remove()
         parent.insertBefore(postCommentForm, commentsList)
+        parent.insertBefore(postCommentsRules, commentsList)
     }
 
 }
